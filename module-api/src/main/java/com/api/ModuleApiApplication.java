e07d2ff7ffc8c0e3e990ced8e0646ce7502dbead
panshanyou <panshanyou@315che.com>
2022-09-22 14:56:28 +0800

    init

--- module-api/src/main/java/com/api/ModuleApiApplication.java
+++ module-api/src/main/java/com/api/ModuleApiApplication.java
@@ -1,8 +1,12 @@
 package com.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.api.*")
+@MapperScan("com.mapper.mapper")//扫描接口类
 @SpringBootApplication
 public class ModuleApiApplication {
 
--- module-api/src/main/java/com/api/controller/IndexController.java
+++ module-api/src/main/java/com/api/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.api.controller;
 
+import com.mapper.entity.TbUser;
+import com.mapper.mapper.TbUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class IndexController {
+    @Autowired
+    private TbUserMapper tbUserMapper;
+
     @RequestMapping("/")
     public String index() {
+        TbUser tbUser = tbUserMapper.selectByPrimaryKey(5);
+        System.out.println(tbUser.getUsername());
         return "hello springboot module";
     }
 }
